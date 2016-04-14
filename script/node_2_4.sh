42b7f17c278d6c162e908b577500a19a39327ced
zhangy10 <zac.lhjzyzzoo@gmail.com>
2016-04-15 01:45:25 +1000

    remove files

--- script/node_2_4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=01:00:00
-#SBATCH --nodes=2
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=1
-module load Java/1.8.0_71
-module load mpj/0.44
-javac -cp .:$MPJ_HOME/lib/mpj.jar Spartan.java Message.java PatternMatch.java Processors.java ReadFileTask.java Occurence.java Settings.java 
-mpjrun.sh -np 8 Spartan Melbourne
